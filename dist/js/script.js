7f3914ebedbfe1e19bdfe5770b5830742c59d3e4
Moro <101040042+Yaichevs@users.noreply.github.com>
2022-10-30 09:59:01 +0300

    добавил счетчик скрипт

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,14 +2,19 @@ const hamburger = document.querySelector('.hamburger'),
         menu = document.querySelector('.menu'),
         closeElem = document.querySelector('.menu__close');
 
-var closeEsc = true;
-
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+const counter = document.querySelectorAll('.subjects__skillElem-pir'),
+        linesNow = document.querySelectorAll('.subjects__scaleNow');
+
+counter.forEach((item, i) => {
+    linesNow[i].style.width = item.innerHTML;
 });
 
 
